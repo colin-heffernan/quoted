a866990ba868d7bc0e38ebaba3e994cf0457cbbd
Colin Heffernan <colinpheffernan@gmail.com>
2024-02-10 13:25:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quoted
+
+Quoted is a simple program that displays a random quote from a JSON list in your .config.
+
+
+# License
+
+Quoted is licensed under the MIT License.
+Quoted uses the Dirs library under the terms of the MIT License.
+Quoted uses the Rand library under the terms of the MIT License.
+Quoted uses the Serde library under the terms of the MIT License.
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+#![warn(clippy::all, clippy::pedantic)]
+use dirs::config_dir;
+use std::fs::read_to_string;
+
+fn main() {
+    let config = config_dir();
+    if config.is_none() {
+        println!("The config directory does not exist or is not accessible.");
+        return;
+    }
+    let mut config = config.unwrap();
+    config.push("quoted");
+    config.push("config");
+    config.set_extension("json");
+    let table_raw =
+        read_to_string(config).expect("Quoted's config file does not exist or is not accessible.");
+    let quotes: Vec<(String, String)> = serde_json::from_str(&table_raw).unwrap();
+    let rand = rand::random::<f64>() * quotes.len() as f64;
+    let (quote, author) = &quotes[rand.floor() as usize];
+    let mut quote_lines: Vec<String> = Vec::new();
+    let mut quote_line = String::from("|");
+    for quote_word in quote.split(' ') {
+        if format!("{} {quote_word}", &quote_line).len() > 50 || quote_word.contains('\n') {
+            quote_lines.push(quote_line.clone());
+            quote_line = String::from("|");
+        }
+        if !quote_word.contains('\n') {
+            quote_line = format!("{} {quote_word}", &quote_line);
+        }
+    }
+    quote_lines.push(quote_line);
+    let mut author_lines: Vec<String> = Vec::new();
+    let mut author_line = String::from(" -");
+    for author_word in author.split(' ') {
+        if format!("{} {author_word}", &author_line).len() > 50 || author_word.contains('\n') {
+            author_lines.push(author_line.clone());
+            author_line = String::from("  ");
+        }
+        if !author_word.contains('\n') {
+            author_line = format!("{} {author_word}", &author_line);
+        }
+    }
+    author_lines.push(author_line);
+    for line in quote_lines {
+        println!("{line}");
+    }
+    for line in author_lines {
+        println!("{line}");
+    }
+}
